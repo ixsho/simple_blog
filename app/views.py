8d4a65bf0dfa07f66300bbecbea70186b65ce143
sho <ikeguchi.sho@gmail.com>
2021-06-15 23:39:14 +0900

    Add: foundation

--- app/views.py
+++ app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.paginator import Paginator
+from app.models import Article
 
-# Create your views here.
+def home(request):
+    obj = Article.objects.all()[:3]
+    context = {
+        'title': 'Simple blog',
+        'articles': obj,
+    }
+    return render(request, 'blog/home.html', context)
+
+def index(request):
+    obj = Article.objects.all()
+    paginator = Paginator(obj, 2)
+    page_number = request.GET.get('page')
+    context = {
+        'page_obj': paginator.get_page(page_number),
+        'page_number': page_number,
+    }
+    return render(request, 'blog/index.html', context)
+
+def article(request, id):
+    obj = Article.objects.get(id=id)
+    context = {
+        'article': obj,
+    }
+    return render(request, 'blog/article.html', context)
--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
+    path('index/', views.index),
+    path('article/<slug:id>/', views.article),
 ]
